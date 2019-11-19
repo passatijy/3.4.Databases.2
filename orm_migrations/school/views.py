777656f16fa3133cbc0e6842db970d35ba3a35f8
passatijy <lmar@mail.ru>
2019-11-19 23:46:38 +0300

    something wrong with students

--- orm_migrations/school/views.py
+++ orm_migrations/school/views.py
@@ -8,9 +8,10 @@ def students_list(request):
     template = 'school/students_list.html'
     students = Student.objects.all()
     context = {'object_list':students}
+    
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
     ordering = 'group'
 
-    return render(request, template, context) 
+    return render(request, template, context)
